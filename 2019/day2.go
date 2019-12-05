26cc7b4439b6df2a11feca7be270b77951420822
Craig Peterson <captncraig@users.noreply.github.com>
2019-12-05 10:42:49 -0500

    d2p2

--- 2019/day2.go
+++ 2019/day2.go
@@ -1,19 +1,39 @@
 package main
 
+import "fmt"
+
 func init() {
 	days[2] = d2
 }
 
 func d2() {
 	digs := inputCommaInts()
-	v(digs)
-	prog := intProg{data: digs}
-	v(prog)
-	for {
-		op := prog.step()
-		v(op, prog)
-		if op == 99 {
-			break
+	newSlice := make([]int, len(digs))
+	exe := func(n, v int) int {
+		copy(newSlice, digs)
+		prog := intProg{data: newSlice}
+		prog.data[1] = n
+		prog.data[2] = v
+		for {
+			op := prog.step()
+			if op == 99 {
+				break
+			}
+		}
+		return prog.data[0]
+	}
+	if !*p2 {
+		fmt.Println(">>>>>", exe(12, 2))
+		return
+	}
+	goal := 19690720
+	for n := 0; n <= 99; n++ {
+		for v := 0; v <= 99; v++ {
+			x := exe(n, v)
+			if x == goal {
+				fmt.Println(">>>>>", n, v, 100*n+v)
+				return
+			}
 		}
 	}
 }
