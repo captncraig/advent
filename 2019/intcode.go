3d82e819f4b14ba5bf0288217b0fccc4e39164a4
Craig Peterson <captncraig@users.noreply.github.com>
2019-12-05 10:33:07 -0500

    d5p2

--- 2019/intcode.go
+++ 2019/intcode.go
@@ -34,7 +34,7 @@ func (i *intProg) step() int {
 	op := i.data[i.pc]
 	adv := 1
 	v(op%10, "!!!", op)
-	switch op % 10 {
+	switch op % 100 {
 	case 1: //+
 		a1 := i.param(1, op)
 		a2 := i.param(2, op)
@@ -51,11 +51,60 @@ func (i *intProg) step() int {
 		r := i.data[i.pc+1]
 		adv = 2
 		i.data[r] = 1 // hard coded
+		if *p2 {
+			i.data[r] = 5
+		}
 	case 4: //OUT
 		a1 := i.param(1, op)
 		fmt.Println(a1, "OOOOOOOOOOOOOO")
 		adv = 2
+
+	case 5: // JNZ
+		// Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer
+		// to the value from the second parameter. Otherwise, it does nothing.
+		a1 := i.param(1, op)
+		r := i.param(2, op)
+		adv = 3
+		if a1 != 0 {
+			i.pc = r
+			adv = 0
+		}
+	case 6: // JZ
+		// Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer
+		// to the value from the second parameter. Otherwise, it does nothing.
+		a1 := i.param(1, op)
+		r := i.param(2, op)
+		adv = 3
+		if a1 == 0 {
+			i.pc = r
+			adv = 0
+		}
+	case 7: // LT
+		// Opcode 7 is less than: if the first parameter is less than the second parameter,
+		// it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+		a1 := i.param(1, op)
+		a2 := i.param(2, op)
+		r := i.data[i.pc+3]
+		if a1 < a2 {
+			i.data[r] = 1
+		} else {
+			i.data[r] = 0
+		}
+		adv = 4
+	case 8:
+		// Opcode 8 is equals: if the first parameter is equal to the second parameter,
+		// it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+		a1 := i.param(1, op)
+		a2 := i.param(2, op)
+		r := i.data[i.pc+3]
+		if a1 == a2 {
+			i.data[r] = 1
+		} else {
+			i.data[r] = 0
+		}
+		adv = 4
 	case 99:
+		v("HALT")
 	default:
 		log.Fatalf("unkown opcode %d", op)
 	}
